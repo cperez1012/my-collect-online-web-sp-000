99d02b0f100f52b15bf85aa4cdb43467772ff653
Christian Perez <perezchristian1012@gmail.com>
2019-07-25 11:35:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,16 @@
-empty_array = []
-
-  def my_collect(empty_array)
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array << yield(array[i].upcase)
+      i += 1
   end
+else
+  "Hey! No block was given!"
+end
+new_array
+end
 
 languages = ["ruby", "javascript", "python", "objective-c"]
 def my_collect(languages)
