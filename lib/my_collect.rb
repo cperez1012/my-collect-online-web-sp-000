1dc20572074b73c3c3058b4c8507d2df83f1fcf4
Christian Perez <perezchristian1012@gmail.com>
2019-07-25 11:27:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,10 @@ empty_array = []
   def my_collect(empty_array)
   end
 
+languages = ["ruby", "javascript", "python", "objective-c"]
 def my_collect(languages)
   end
 
+students = []
 def my_collect(students)
   end
